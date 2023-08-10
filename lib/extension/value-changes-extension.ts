540b7700f39702fc87ad67917ab48dd0f87acac6
m310851010 <310851010@qq.com>
2023-08-10 22:07:39 +0800

    fixed: 多次触发valueChanges

--- lib/extension/value-changes-extension.ts
+++ lib/extension/value-changes-extension.ts
@@ -2,15 +2,24 @@ import { FormlyExtension } from '@ngx-formly/core';
 
 /**
  * 自动添加valueChanges函数
+ * @example
+ * fields = [
+ * {
+ *  type: 'input',
+ *  props: {
+ *    valueChanges: (value, field) => {}
+ *  }
+ * }
+ * ]
  */
 export const valueChangesExtension: FormlyExtension = {
   postPopulate(field): void {
-    if (!field.props?.valueChanges) {
+    if (!field.formControl || !field.props?.valueChanges || field.props.valueChanges.observed) {
       return;
     }
-    if (field.formControl) {
-      field.formControl.valueChanges.subscribe(value => field.props?.valueChanges(value, field));
-    }
+
+    field.props.valueChanges.observed = true;
+    field.formControl.valueChanges.subscribe(value => field.props!.valueChanges(value, field));
   }
 };
 
